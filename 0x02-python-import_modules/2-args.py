c3c8ee3c4e5f00efcbf47d304465cc300b35944a
NadaHusein <nada.hussein796@gmail.com>
2023-11-06 03:15:52 -0800

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    '''
-    
-    Number of argument(s) followed by argument (if number is one) or arguments (otherwise), followed by
-    : (or . if no arguments were passed) followed by
-    a new line, followed by (if at least one argument),
-    one line per argument:
-        the position of the argument (starting at 1) followed by :, followed by the argument value and a new line
-'''
 
     count = len(sys.argv) - 1
     if (count == 0):
