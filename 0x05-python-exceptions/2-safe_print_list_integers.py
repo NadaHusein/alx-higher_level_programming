0a0d0d6e8f6e27eed135eb9f48f5df15551f246e
NadaHusein <nada.hussein796@gmail.com>
2023-11-21 02:02:40 -0800

    mendatory

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i, c = 0,0
+    while i < x:
+        try:
+            print('{:d}'.format(my_list[i]))
+            c+=1
+        except (TypeError, ValueError):
+            pass
+        i += 1
+    print()
+    return c
+    
